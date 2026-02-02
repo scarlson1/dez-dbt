647bdcaf396f1704fe36015a3661549c7bb24842
Spencer Carlson <19680614+scarlson1@users.noreply.github.com>
2026-02-02 17:48:38 -0600

    macros & seed

--- models/marts/dim_locations.sql
+++ models/marts/dim_locations.sql
@@ -1,1 +1,14 @@
-select 1
+with taxi_zone_lookup as (
+  select * from {{ ref('taxi_zone_lookup') }}
+),
+
+rename as (
+  select 
+    locationid as location_id,
+    borough,
+    zone,
+    service_zone
+  from taxi_zone_lookup
+)
+
+select * from taxi_zone_lookup
--- models/marts/fct_trips.sql
+++ models/marts/fct_trips.sql
@@ -1,1 +1,9 @@
+/*
+  - one row per trip
+  - add a primary key (trip id) unique
+  - find all the duplicates, understand why they happen, and fix
+  - find a why to enrich the column payment_type
+*/
+
+
 select 1
